56fd13da8ad61acf8ffa834840cc8395245807e1
DeeTomPanda <thewalkerdarkknight@gmail.com>
2022-06-23 23:23:52 +0530

    Add files via upload

---
+++ hindustan_web_scraper.js
@@ -0,0 +1,34 @@
+const puppeteer=require('puppeteer');
+const $=require('cheerio');
+
+var titles=[];
+
+async function main()
+{
+   await puppeteer.launch({headless:false})
+	
+	.then(async (browser)=>{
+              await browser.newPage()
+	
+	      .then(async(page)=>{
+		   await page.goto("https://www.hindustantimes.com/india-news")
+	           return page.content();
+	       })
+
+	      .then((html_content)=>{
+	
+		   $(".hdg3 >a",html_content).each(function(){ 
+			   titles.push($(this).text());             //this refers to the html element here..
+		   })                                                //Also this is accompanied by a jQuery wrapper,so
+	      })
+	      
+	      .then(()=>{                                                     //we can use "this" to reference the outer obj...
+              for (var i=0;i<titles.length;i++)                      //Note"Does not work for non-jQuery objects/funcs
+		 console.log(` ${i} ${titles[i]} \n`);
+              });
+        
+        });
+}
+
+main();
+
